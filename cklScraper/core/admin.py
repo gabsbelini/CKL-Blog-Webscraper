99cf6488fcee812c6c1343ca7f040952c5cab9f6
gabsbelini <gabs_belini@hotmail.com>
2017-10-18 18:50:34 -0200

    Article e Author model criados

--- cklScraper/core/admin.py
+++ cklScraper/core/admin.py
@@ -1,4 +1,15 @@
 from django.contrib import admin
-from .models import Subject
+from .models import Subject, Author, Article
 
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'subject', 'publish_date', 'author']
+
+
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Author, AuthorAdmin)
 admin.site.register(Subject)
--- cklScraper/core/models.py
+++ cklScraper/core/models.py
@@ -4,4 +4,27 @@ from django.db import models
 class Subject(models.Model):
     name = models.CharField('Name', max_length=150)
     color = models.CharField('Color', max_length=50)
-    
+
+    def __str__(self):
+        return self.name
+
+
+class Author(models.Model):
+    name = models.CharField('Name', max_length=150)
+    picture = models.ImageField(upload_to='pictures')
+
+    def __str__(self):
+        return self.name
+
+
+class Article(models.Model):
+    title = models.CharField('Title', max_length=300)
+    slug = models.CharField('Slug', max_length=100)
+    author = models.ForeignKey('core.Author', verbose_name='Author Name')
+    subject = models.ForeignKey('core.Subject', verbose_name='Subject')
+    hero_image = models.ImageField(upload_to='pictures')
+    publish_date = models.DateField()
+    text = models.TextField()
+
+    def __str__(self):
+        return self.title
