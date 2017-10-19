8223a481c6827a4d1dc3efcaf36726c46cf87556
gabsbelini <gabs_belini@hotmail.com>
2017-10-18 22:18:45 -0200

    Getting titles, names, tags, time, and text

--- cklScraper/core/views.py
+++ cklScraper/core/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
-# Create your views here.
+from bs4 import BeautifulSoup
+import urllib
+
+def index(request):
+    print('----------------TITLES-------------')
+    html = urllib.request.urlopen('https://techcrunch.com/')
+    soup = BeautifulSoup(html, 'html.parser')
+    for anchor in soup.find_all('h2', attrs={'class': 'post-title'}):
+        print(anchor.text)
+    print('----------------NAMES-------------')
+    for name in soup.find_all('a', attrs={'rel': 'author'}):
+        print(name.text)
+    print('----------------TAGS-------------')
+    for tag in soup.find_all('a', attrs={'class': 'tag'}):
+        print(tag.text)
+    print('----------------TIMES-------------')
+    for time in soup.find_all('time'):
+        print(time['datetime'])
+    for text in soup.find_all('p', attrs={'class': 'excerpt'}):
+        print(text.text[:200])
+        print()
+    return render(request, 'index.html')
