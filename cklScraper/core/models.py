d943fa851f7bdac8d1d5f7586b9429ef9c6f0318
gabsbelini <gabs_belini@hotmail.com>
2017-10-20 05:58:45 -0200

    refactored data extraction method

--- cklScraper/core/models.py
+++ cklScraper/core/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Subject(models.Model):
     name = models.CharField('Name', max_length=150)
-    color = models.CharField('Color', max_length=50)
+    color = models.CharField('Color', max_length=50, default='None')
 
     def __str__(self):
         return self.name
@@ -19,6 +19,7 @@ class Author(models.Model):
 
 class Article(models.Model):
     title = models.CharField('Title', max_length=300)
+    url = models.CharField('URL', max_length=200)
     slug = models.CharField('Slug', max_length=100)
     author = models.ForeignKey('core.Author', verbose_name='Author Name')
     subject = models.ForeignKey('core.Subject', verbose_name='Subject')
--- cklScraper/core/views.py
+++ cklScraper/core/views.py
@@ -6,59 +6,47 @@ from bs4 import BeautifulSoup
 import urllib
 import json
 import os
+import re
 
 
 def index(request):
-    titles_list, authors_list, tags_list, dates_list, texts_list, imgs_list = [], [], [], [], [], []
-    tags_list.append('LALALA')
-    html = urllib.request.urlopen('https://techcrunch.com/')
+    html = urllib.request.urlopen('https://techcrunch.com/page/2')
     soup = BeautifulSoup(html, 'html.parser')
+    blocks = [block for block in soup.find_all('li', attrs={'class': 'river-block'})]
 
-    for anchor in soup.find_all('h2', attrs={'class': 'post-title'}):
-        titles_list.append(anchor.text)
-    for name in soup.find_all('a', attrs={'rel': 'author'}):
-        authors_list.append(name.text)
-    for tag in soup.find_all('div', attrs={'class': 'tags'}):
-        tags_list.append(tag.text.strip('\n'))
-    for dates in soup.find_all('time', attrs={'class': 'timestamp'}):
-        dates_list.append(dates['datetime'])
-    for text_summary in soup.find_all('p', attrs={'class': 'excerpt'}):
-        texts_list.append(text_summary.text[:200])
-    for img in soup.find_all('img'):
-        if 'data-src' in img.attrs:
-            if img['data-src'].startswith('https://tctechcrunch20'):
-                imgs_list.append(img['data-src'])
-    print(tags_list)
-    print(len(titles_list))
-    print(len(authors_list))
-    print(len(tags_list))
-    print(len(dates_list))
-    print(len(texts_list))
-    print(len(imgs_list))
-
-    for x in range(len(titles_list)):
-        if not Article.objects.filter(title=titles_list[x]).exists():
+    def extract_data():
+        for block in blocks:
+            # Checks if article has already been added
+            if Article.objects.filter(title=block.find('h2').text).exists():
+                continue
             article = Article()
-            article.title = titles_list[x]
-            if not Subject.objects.filter(name=tags_list[x]).exists():
-                subject = Subject()
-                subject.name = tags_list[x]
-                subject.save()
-                article.subject = Subject.objects.latest('id')
-            else:
-                print(tags_list[x])
-                article.subject = Subject.objects.get(name=tags_list[x])
-            if not Author.objects.filter(name=authors_list[x]).exists():
-                author = Author()
-                author.name = authors_list[x]
+            article.title = block.find('h2').text
+            article.url = block['data-permalink']
+            article.slug = re.findall('https://techcrunch.com/[0-9]{4}/[0-9]{2}/[0-9]{2}/(.*)/', block['data-permalink'])[0]
+
+            article.publish_date = block.find('time')['datetime']
+            article.text = block.find('p', attrs={'class': 'excerpt'}).text[:200]
+            # Checks if author isn't in database yet and adds it
+            if not Author.objects.filter(name=block.find('a', attrs={'rel': 'author'}).text).exists():
+                author = Author(name=block.find('a', attrs={'rel': 'author'}).text)
                 author.save()
                 article.author = Author.objects.latest('id')
             else:
-                article.author = Author.objects.get(name=authors_list[x])
-            article.publish_date = dates_list[x]
-            article.text = texts_list[x]
-            temp_image = urllib.request.urlretrieve(imgs_list[x])
-            article.hero_image.save(os.path.basename(imgs_list[x]), File(open(temp_image[0], 'rb')))
+                article.author = Author.objects.get(name=block.find('a', attrs={'rel': 'author'}).text)
+            # Checks if article has a subject
+            if block.find('a', attrs={'class': 'tag'}):
+                if not Subject.objects.filter(name=block.find('a', attrs={'class': 'tag'}).text).exists():
+                    subject = Subject(name=block.find('a', attrs={'class': 'tag'}).text)
+                    subject.save()
+                    article.subject = Subject.objects.latest('id')
+                else:
+                    article.subject = Subject.objects.get(name=block.find('a', attrs={'class': 'tag'}).text)
+            else:
+                article.subject = Subject.objects.get(name='null')
+            if block.find('img'):
+                temp_image = urllib.request.urlretrieve(block.find('img')['data-src'])
+                article.hero_image.save(os.path.basename(block.find('img')['data-src']), File(open(temp_image[0], 'rb')))
             article.save()
 
-    return render(request, 'index.html')
+    extract_data()
+    return render(request, 'index.html', {'data': data})
